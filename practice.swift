df6ef5c5a13c7fdf1f79251f319fbdd5311f28ad
kimsjsj <86788022+kimsjsj@users.noreply.github.com>
2021-10-23 17:15:19 +0900

    Create practice.swift

---
+++ practice.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  sj_1023
+//
+//  Created by Sejin on 2021/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func ChnBackground(_ sender: Any) {
+        view.backgroundColor = UIColor.black   }
+    
+}
+
